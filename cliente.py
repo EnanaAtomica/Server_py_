cbffe969930be175acd144f28b0dae2395ec0ce1
EnanaAtomica <zcabrera@lasalleflorida.edu.ar>
2024-06-13 15:04:17 -0300

    noc de cuando es

--- cliente.py
+++ cliente.py
@@ -1,10 +1,12 @@
 import socket
 
-# Creamos un socket TCP/IP
+#conexion UDP
+#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#conexion TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
-s.connect(('localhost', 12345)) 
+s.connect(('192.168.180.237', 12345)) 
 s.send("Hola, servidor".encode())
 
 response = s.recv(1024).decode()
--- servidor.py
+++ servidor.py
@@ -1,12 +1,32 @@
 import socket 
 
+ip = '192.168.180.237'
+puerto = 2200
+
+#conexion TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost',12345))
-s.listen ()
 
-conn, addr = s.accept ()
-data = conn.recv(1024)
+s.bind((ip, puerto))
+print(f"escuchando en el ip: {ip}, puerto: {puerto}")
+s.listen()
 
-conn.sendall ("Message for client".encode())
+conn, addr = s.accept()
+data = conn.recv (1024)
 
-print("programa funca")
+conn.sendall("Message for client".encode())
+
+print(data)
+
+s.close()
+#conexion UDP
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.bind(('localhost',12345))
+
+
+# data, addr = s.recvfrom (1024)
+
+# s.sendto("Message for client".encode(),addr)
+
+# print("programa funca")
+# print(data)
+# s.close()
